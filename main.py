a373f6911ab62c98a6c1a8ccbd70058ea9094ddf
Sean <sean@Seans-MacBook-Air.local>
2024-09-12 11:20:45 +0800

    Refactor code to use configuration file and email sender

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import time
-#from dotenv import load_dotenv
-import os
 
+from config import Config
+from email_sender import send_email
 
 class CourseSelector:
     def __init__(self, driver_path, url, account, password):
-        # Set up logging
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
         handler = logging.StreamHandler()
@@ -23,16 +22,13 @@ class CourseSelector:
         self.service = Service(driver_path)
 
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')  # 啟用無介面模式
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(service=self.service, options=chrome_options)
         
         self.url = url
-        #load_dotenv()
-        #self.account = os.getenv('ACCOUNT')
-        #self.password = os.getenv('PASSWORD')
         self.account = account
         self.password = password
 
@@ -87,7 +83,39 @@ class CourseSelector:
 
         submit_button = self.driver.find_element(By.ID, 'Button1')
         submit_button.click()
+    
+    def filter_courses(self, time_span='', course_name_exp=''):
+        """
+        Filter courses based on time span and/or course name.
+        :param time_span: The desired time span for the course
+        :param course_name_exp: The desired course name (or part of it)
+        :return: List of filtered courses
+        """
+        self.driver.implicitly_wait(1)
+        records = self.driver.find_elements(By.CSS_SELECTOR, "tr.record2, tr.hi_line")
+        filtered_records = records[::2]  # Select every other record to avoid duplicates
+        
+        self.logger.debug(f'Total courses: {len(filtered_records)}')
+        
+        results = []
+        for record in filtered_records:
+            course_name = record.find_element(By.XPATH, './td[4]/a[1]').text
+            span_value = record.find_element(By.XPATH, './td[6]/span').text
+            font_value = record.find_element(By.XPATH, './td[8]/font').text
+            
+            if self._should_include_course(record, time_span, course_name_exp):
+                self.logger.info(f'Filtered Course: {course_name}, Time: {span_value}, Availability: {font_value}')
+                results.append([course_name, font_value, span_value])
+            else:
+                self.logger.debug(f"Course: {course_name}, Time: {span_value}, Availability: {font_value}")
+        
+        return results
 
+    def _should_include_course(self, record, time_span, course_name_exp):
+        return (self._filter_by_time_span(record, time_span) and
+                self._filter_by_course_name(record, course_name_exp) and
+                self._is_course_available(record))
+    
     def _filter_by_time_span(self, record, time_span):
         """Filter a record based on the given time span."""
         span_value = record.find_element(By.XPATH, './td[6]/span').text
@@ -104,65 +132,30 @@ class CourseSelector:
         comp_font_value = font_value.split('/')
         return comp_font_value[0] != comp_font_value[1]
     
-    def filter_courses(self, time_span='\n', course_name_exp=''):
-        """
-        Filter courses based on time span and/or course name.
-        
-        :param time_span: The desired time span for the course
-        :param course_name: The desired course name (or part of it)
-        :return: List of filtered courses
-        """
-        self.driver.implicitly_wait(1)
-        records = self.driver.find_elements(By.CSS_SELECTOR, "tr.record2, tr.hi_line")
-        filtered_records = [record for index, record in enumerate(records) if index % 2 == 0]
-        
-        self.logger.info(f'Total courses: {len(filtered_records)}')
-        
-        # Log all courses for debugging
-        for record in filtered_records:
-            span_value = record.find_element(By.XPATH, './td[6]/span').text
-            font_value = record.find_element(By.XPATH, './td[8]/font').text
-            course_name = record.find_element(By.XPATH, './td[4]/a[1]').text
-            if time_span in span_value or course_name_exp in course_name:
-                self.logger.debug(f"Course: {course_name}, Time: {span_value}, Availability: {font_value}")
-        
-        results = []
-        for record in filtered_records:
-            if (self._filter_by_time_span(record, time_span) and
-                self._filter_by_course_name(record, course_name_exp) and
-                self._is_course_available(record)):
-                
-                course_name = record.find_element(By.XPATH, './td[4]/a[1]').text
-                span_value = record.find_element(By.XPATH, './td[6]/span').text
-                font_value = record.find_element(By.XPATH, './td[8]/font').text
-                
-                self.logger.info(f'Filtered Course: {course_name}, Time: {span_value}, Availability: {font_value}')
-                results.append([course_name, font_value, span_value])
-        
-        return results
-    
     def store_result(self, results):
         message_templet = """目前釋出名額有：\n"""
-        with open('result.txt', 'r+') as f:
-            for result in results:
-                if result[0] in f:
-                    continue
-                f.write(f"{result[0]} {result[1]} {result[2]}\n")
-                message_templet += "課程名稱：" + result[0] + "\n目前人數：" + result[1] + "\n時段：" + result[2] + "\n"
+        # with open('result.txt', 'r+') as f:
+        for result in results:
+            # if result[0] in f:
+            #     continue
+            # f.write(f"{result[0]} {result[1]} {result[2]}\n")
+            message_templet += "課程名稱：" + result[0] + "\n目前人數：" + result[1] + "\n時段：" + result[2] + "\n"
         return message_templet
 
     def close(self):
         self.driver.quit()
 
-# 使用方式
+logger = logging.getLogger(__name__)
 
 def main():
-    logger = logging.getLogger(__name__)
-    driver_path = '/Users/sean/chromedriver-mac-arm64/chromedriver'
-    url = 'https://portalfun.yzu.edu.tw/cosSelect/index.aspx'
-    #account = os.getenv('ACCOUNT')
-    #password = os.getenv('PASSWORD')
+    config = Config()
 
+    if logger.handlers:
+        logger.handlers.clear()
+    url = 'https://portalfun.yzu.edu.tw/cosSelect/index.aspx'
+    driver_path = config.args.driver_path
+    account = config.args.username
+    password = config.args.password
 
     selector = CourseSelector(driver_path, url, account, password)
     selector.open_page()
@@ -172,19 +165,41 @@ def main():
         logger.debug(f"CheckCode: {check_code}")
 
         selector.login(check_code)
-        # selector.select_courses('113,1  ', '904', '2') #興趣體育
-        # results = selector.filter_courses()
-        
-        selector.select_courses(year_value='113,1  ', dept_value='700', degree_value='1')
-        results = selector.filter_courses(time_span="203", course_name_exp="基礎程式設計-C++實習")
 
+        # 興趣體育
+        coures_name = "體育"
+        selector.select_courses('113,1  ', '904', '2') 
+        results = selector.filter_courses()
+        
+        # 自訂
+        # coures_name = "網路安全"
+        # selector.select_courses(year_value='113,1  ', dept_value='701', degree_value='3')
+        # results = selector.filter_courses(time_span="308", course_name_exp=coures_name)
+
+
+        if results:
+            logger.info("有課程")
+            send_email(
+                subject=f"{coures_name}課程有空位!!",
+                body="請盡快登入選課系統選課",
+                sender_email=config.args.sender_email,
+                password=config.args.sender_password,
+                receiver_email=config.args.receiver_email,
+                # cc_emails=[],
+            )
+        else:
+            logger.info("沒有課程")
+            selector.close()
+            return
 
         message_templet = selector.store_result(results)
-        print(message_templet)
+        logger.info(message_templet)
     else:
         logger.error("未能找到 CheckCode cookie")
 
     selector.close()
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
+        time.sleep(300)
